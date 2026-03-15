1e10b301dc49a4722b4250fecf2f59a3bfc523ab
haruhi1tengokudaimokyo <2902262868@qq.com>
2026-03-15 18:02:44 +0800

    实现登录后端

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,11 +1,14 @@
+
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 from web.views.index import index
+from web.views.user.accout.login import LoginView
+from web.views.user.accout.logout import LogoutView
+from web.views.user.accout.register import RegisterView
+from web.views.user.accout.refresh_token import RefreshTokenView
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/user/account/login/',LoginView.as_view()),
+    path('api/user/account/logout/',LogoutView.as_view()),
+    path('api/user/account/register/',RegisterView.as_view()),
+    path('api/user/account/refresh_token/',RefreshTokenView.as_view()),
     path('',index),
 ]
